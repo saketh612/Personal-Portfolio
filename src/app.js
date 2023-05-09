de6530f5ebf5d91c44eecdc88b64f3da13349c81
Saketh <saketh.repaka01@gmail.com>
2023-05-09 13:25:17 +0000

    Commit All

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/index.html'));
 })
 
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.join(__dirname,'pages/contact.html'));
+})
+
+app.get('/CV',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/SAF.pdf'));
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
